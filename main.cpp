40713b6378f6d2449a910628e53fc7282d5842c3
engineer1995 <skkameika@ya.ru>
2023-09-26 22:30:12 +0300

    Added SelectionSort

--- main.cpp
+++ main.cpp
@@ -1,21 +1,40 @@
 #include <iostream>
 
 void BubbleSort(int* arr, size_t N);
-void SelectionSort();
-void InsertSort();
+void SelectionSort(int* arr, size_t N);
+void InsertSort(int* arr, size_t N);
 
 
 
 int main(){
 
-    int arr [] = {23,43,1, 4, 5};
+    int arr[] = {123, 122, 120, 120, 0};
     int N = 5;
-    BubbleSort(arr, N);
+    SelectionSort(arr, N);
     for(int i = 0; i < N; ++i){
         std::cout << arr[i] << " ";
     }
+return 0;
 }
 
+
+
+void SelectionSort(int* arr, size_t N){
+    for (int i = 0; i < N-1; ++i){
+        int buffer = i;
+        for(int j = i+1; j < N; ++j){
+            if(arr[buffer] > arr[j]){
+                buffer = j;
+            }
+        }
+        if(buffer!=i){
+            int temp = arr[i];
+            arr[i] = arr[buffer];
+            arr[buffer] = temp;
+            
+        }
+    }
+}
 
 
 void BubbleSort(int* arr, size_t N){
